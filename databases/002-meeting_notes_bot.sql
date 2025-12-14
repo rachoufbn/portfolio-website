b30477bddd436eeecb55b96079510066341912d3
Rachelle Fabian <rachelle.ef.mail@gmail.com>
2025-12-14 22:11:08 +0100

    fix: default MySQL table schema initialisation: including collation, foreign keys and removing default auto increment value

--- databases/002-meeting_notes_bot.sql
+++ databases/002-meeting_notes_bot.sql
@@ -9,7 +9,7 @@ CREATE TABLE `users` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 
 CREATE TABLE `meetings` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -21,7 +21,7 @@ CREATE TABLE `meetings` (
   UNIQUE KEY `unique_title` (`title`),
   KEY `user_idx` (`user_id`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 
 CREATE TABLE `notes` (
   `meeting_id` int NOT NULL,
@@ -41,9 +41,8 @@ CREATE TABLE `chat_history` (
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `notes_version` int NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `notes_version_idx` (`notes_version`),
-  KEY `meeting_id_idx` (`meeting_id`),
-  KEY `meeting_id` (`meeting_id`) /*!80000 INVISIBLE */,
+  KEY `meeting_id_idx` (`meeting_id`) /*!80000 INVISIBLE */,
+  KEY `notes_idx` (`meeting_id`,`notes_version`),
   CONSTRAINT `meeting_id_2` FOREIGN KEY (`meeting_id`) REFERENCES `meetings` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
-  CONSTRAINT `notes_version` FOREIGN KEY (`notes_version`) REFERENCES `notes` (`version`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  CONSTRAINT `notes` FOREIGN KEY (`meeting_id`, `notes_version`) REFERENCES `notes` (`meeting_id`, `version`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
